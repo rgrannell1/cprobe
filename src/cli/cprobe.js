34ac3a7486d82c897044cf35b1d0d7b85e1fba83
rgrannell1 <r.grannell2@gmail.com>
2016-04-30 19:26:05 +0100

    updated documentation

--- src/cli/cprobe.js
+++ src/cli/cprobe.js
@@ -6,6 +6,7 @@
 
 
 
+const colors    = require('colors')
 const constants = require('../commons/constants')
 
 
@@ -14,20 +15,32 @@ const constants = require('../commons/constants')
 
 const docs = `
 Name:
-	cprobe — monitor the connection status to a URL.
+	cprobe — test network connections to a URL.
 Usage:
 	cprobe [-i <NUM> | --interval <NUM>] [--] <url>...
 	cprobe (-h | --help | --version)
 
 Description:
-	*
+
+	cprobe is a primarily interactive tool for testing connections to provided URLs. Its envisaged uses are to:
+
+	* replace manual CURL testing during installation / reconfiguration.
+	* test and measure network reliability.
 
 Options:
 	-i <NUM>, --interval <NUM>    The probe interval [default: 5].
 	-h, --help                    Display this documentation.
 	--version                     Display the package version.
 Arguments:
-	<url>...      .
+	<url>...                      The URLs to test the connection status of. Each URL must include it's protocol (e.g. http://example.com)
+	                              The folloring protocols are currently supported:
+	                              * http
+	                              * https
+
+
+Authors:
+	${constants.packageJson.author}
+
 Version:
 	v${constants.packageJson.version}
 `
