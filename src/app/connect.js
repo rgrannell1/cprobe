4ca90d5df05bd14b109261166e265f34a50fe747
rgrannell1 <r.grannell2@gmail.com>
2016-04-30 22:36:06 +0100

    Updated SSH loading; fixed name error

--- src/app/connect.js
+++ src/app/connect.js
@@ -43,8 +43,7 @@ connect.ssh = connData => {
 	return new Promise((resolve, reject) => {
 
 		const start = process.hrtime( )
-
-		const conn = new sshRequest( )
+		const conn  = new sshRequest( )
 
 		conn
 		.on('ready', ( ) => {
@@ -52,26 +51,11 @@ connect.ssh = connData => {
 
 			})
 		})
-		.on('error', err => {
-			console.log(err)
-			reject(err)
-		})
+		.on('error', reject)
 		.connect({
 			host:     connData.hostname,
 			port:     connData.port,
-			username: connData.username
-		})
-
-		httpRequest(connData.url, (err, res, body) => {
-
-			if (err) {
-				return reject(err)
-			}
-
-			res.responseTime = parseInt(process.hrtime(start)[1] / constants.units.nanosecondsPerMillisecond, 10)
-
-			resolve(res, body)
-
+			username: connData.user
 		})
 
 	})
